2b3c9b871100832b38e40539878c2dfa29c91899
Alexander Okhnovsky <alexander-okhnovsky@mail.ru>
2023-03-16 21:05:29 +0300

    db name

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -73,7 +73,7 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.postgresql'),
-        'NAME': os.getenv('DB_NAME', default='foodgramdb'),
+        'NAME': os.getenv('DB_NAME', default='postgres'),
         'USER': os.getenv('POSTGRES_USER', default='postgres'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='Okhnovsky'),
         'HOST': os.getenv('DB_HOST', default='127.0.0.1'),
